42038882de2068ad4c77912cd6844376f884de0e
4nik <shak.iu0@gmail.com>
2025-06-15 14:55:21 +0600

    Setup router

---
+++ src/routes/router.jsx
@@ -0,0 +1,67 @@
+import { createBrowserRouter, Outlet} from 'react-router'
+import Signup from '../pages/Signup'
+import Signin from '../pages/Signin'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <p>MainLayout</p>,
+        children: [
+            {
+                index: true,
+                element: <p>Home</p>
+            },
+            {
+                path: "auth",
+                element: <Outlet />,
+                children: [
+                    {
+                        path: "register",
+                        element: <Signup />
+                    },
+                    {
+                        path: "login",
+                        element: <Signin />
+                    }
+                ]
+            },
+            {
+                path: 'artifacts',
+                element: <Outlet />,
+                children: [
+                    {
+                        index: true,
+                        element: <p>AllArtifacts</p>
+                    },
+                    {
+                        path: 'add',
+                        element: <p>Artifacts</p>
+                    },
+                    {
+                        path: ':id',
+                        element: <p>ArtifactDetails</p>
+                    },
+                    {
+                        path: ':id/edit',
+                        element: <p>UpdateArtifact</p>
+                    }
+                ]
+            },
+            {
+                path: 'liked',
+                element: <p>LikedArtifacts</p>
+            },
+            {
+                path: 'my',
+                element: <p>MyArtifacts</p>
+            }
+        ]
+
+    },
+    {
+        path: "*",
+        element: <p>ErrorLayout</p>
+    }
+])
+
+export default router
